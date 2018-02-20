964c0f0bcc699d6fc1fc595fb9511c1c1e17f649
Rohit Navalgund <rn@Rohits-MacBook-Air.local>
2018-02-20 14:31:41 +0530

    Initial Commit

---
+++ First.s
@@ -0,0 +1,11 @@
+.section __TEXT, __text
+.global _main
+.align 2
+
+push {r7, lr}
+
+mov r1, 0xB
+
+pop{r7, pc}
+
+// Basically Push() and Pop() as in stacks. Hmm, interesting stuff for me as a beginner.
